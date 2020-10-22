f0696642479ee3f52856ccd41204c09137311974
Ayham Koutiney <Ayham.koutiney@hotmail.com>
2020-10-22 21:36:44 +0200

    Displayed updated price in BuildControls Component

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -58,7 +58,8 @@ class BurgerBuilder extends Component{
                 <BuildControls
                     ingredientAdded = {this.addIngredientHandler}
                     ingredientRemoved = {this.removeIngredientHandler}
-                    disabled = {disabledInfo}/>
+                    disabled = {disabledInfo}
+                    price = {this.state.totalPrice}/>
             </Aux>
         );
     }
