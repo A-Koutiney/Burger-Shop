2575dd906c8b7a78ad8fcd4d71269e410a0693af
Ayham Koutiney <Ayham.koutiney@hotmail.com>
2020-10-22 23:04:19 +0200

    Created Backdrop Componant and implemet it in Modal Componant

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -63,6 +63,9 @@ class BurgerBuilder extends Component{
     purchaseHandlar = ()=>{
         this.setState({purchasing:true})
     }
+    purchasecancelHandlar = ()=>{
+        this.setState({purchasing:false})
+    }
     render(){
         const disabledInfo ={
             ...this.state.ingredients
@@ -72,7 +75,7 @@ class BurgerBuilder extends Component{
         }
         return(
             <Aux>
-                <Modal show = {this.state.purchasing}>
+                <Modal show = {this.state.purchasing} modalClosed = {this.purchasecancelHandlar}>
                     <OrderSummary ingredients = {this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
