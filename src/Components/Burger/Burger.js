2964d0b2fff92ebe6983c7ee9c02351d777330e7
Ayham Koutiney <Ayham.koutiney@hotmail.com>
2020-10-22 19:41:42 +0200

    setup Burger Component to recieve dynamic ingredients

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,11 +4,16 @@ import classes from './Burger.css'
 import BurgerBuilder from '../../Containers/BurgerBuilder/BurgerBuilder';
 
 const burger = (props)=>{
+    const trasformedIngredients = Object.keys(props.ingredients)
+        .map(igKey=>{
+            return [...Array(props.ingredients[igKey])].map((_,i)=>{
+                return <BurgerIngredient Key = {igKey + i} type = {igKey}/>
+            });
+        });
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type = "bread-top"/>
-            <BurgerIngredient type = "cheese"/>
-            <BurgerIngredient type = "meat"/>
+            {trasformedIngredients}
             <BurgerIngredient type = "bread-bottom"/>
         </div>
             
